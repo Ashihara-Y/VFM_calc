2ce3b13e34475811b1e47cf040059aaf1fe48c50
Yoshihiro Ashihara <yoshihiro.ashihara@gmail.com>
2026-07-08 00:11:02 +0900

    Change CSV encoding and modify DataFrame processing
    Updated encoding from 'shift_jis' to 'cp932' for reading CSV. Adjusted DataFrame processing and output file handling.

--- BOJ_ExpInflRate_down.py
+++ BOJ_ExpInflRate_down.py
@@ -29,9 +29,9 @@ def run(playwright: Playwright) -> None:
 
     # 2. PandasでDataFrameに読み込み（Shift_JISを指定）
     # ※日銀短観のCSVは上部に英語・日本語のタイトル行があるため、必要に応じて skiprows を調整してください
-    df = pd.read_csv(file_path, encoding='shift_jis', skiprows=1)
+    df = pd.read_csv(file_path, encoding='cp932', skiprows=1)
 
-    # 3. DataFrameの編集（例：列名の変更）
+    # 3. DataFrameの編集
     # 実際のCSV構造に合わせて変更してください
     df = df.rename(columns={
     '物価全般の見通し/５年後/企業の物価見通しの平均/全規模/全産業': '全規模/全産業',
@@ -41,9 +41,13 @@ def run(playwright: Playwright) -> None:
     '物価全般の見通し/５年後/企業の物価見通しの平均/全規模/_対事業所サービス': '全規模/対事業所サービス'
     })
 
-    # 4. UTF-8に変換して新しいファイル（または同じファイル名）に保存
+    df['系列名称'] = pd.to_datetime(df['系列名称'], format='%Y/%m').dt.strftime('%Y年%m月')
+
+    num = len(df['全規模/全産業'].dropna())
+    df = df.iloc(num-1)
+    
+    # 4. UTF-8に変換して新しいファイルに保存
     output_path = "./BOJ_ExpInflRate_utf8.csv"
-    df['系列名称'] = "'" + df['系列名称'].astype(str)
     df.to_csv(output_path, index=False, encoding='utf-8')
 
     page2.close()
